4ab8963005535ade1ee6f1384c5b889b8caefe81
Dan Wilson <danwilson282@yahoo.co.uk>
2023-04-11 17:48:05 +0100

    Initial Commit

--- components/translator.js
+++ components/translator.js
@@ -4,7 +4,70 @@ const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
 class Translator {
-
+    USToGB(text){
+        let translated = ''
+        const wordsArray = {...americanOnly, ...americanToBritishSpelling, ...americanToBritishTitles}
+        translated = this.translate(text, wordsArray)
+        translated = this.time(translated, "US")
+        return translated
+    }
+    GBToUS(text){
+        let translated = ''
+        let GBtoUSTitles = this.swap(americanToBritishTitles)
+        let GBtoUSSpelling = this.swap(americanToBritishSpelling)
+        const wordsArray = {...britishOnly, ...GBtoUSSpelling, ...GBtoUSTitles}
+        translated = this.translate(text, wordsArray)
+        translated = this.time(translated, "GB")
+        return translated
+    }
+    swap(json){
+        var ret = {};
+        for(var key in json){
+          ret[json[key]] = key;
+        }
+        return ret;
+    }
+    time(text, lang){
+        let regex
+        let newDiv
+        let newVal
+        //let regex = /(([0-9]|0[0-9]|1[0-9]|2[0-3])(:|\.)([0-5][0-9]))/g;
+        if (lang=="GB"){
+            regex = /(([0-9]|0[0-9]|1[0-9]|2[0-3])(\.)([0-5][0-9]))/g;
+            newDiv = ['.', ':']
+        }
+        else if (lang=="US"){
+            regex = /(([0-9]|0[0-9]|1[0-9]|2[0-3])(:)([0-5][0-9]))/g;
+            newDiv = [':', '.']
+        }
+        const changeTime = text.match(regex);
+        if (changeTime){
+            changeTime.forEach((value, i)=>{
+                newVal = value.replace(newDiv[0], newDiv[1])
+                console.log(newVal)
+                text = text.replace(value, function (){
+                    return '<span class="highlight">' + newVal + '</span>';
+                })
+            })
+        }
+        
+        return text
+    }
+    translate(text, wordsArray){
+        let regex
+        for (const original in wordsArray) {
+            regex = new RegExp(`${original}`, "gi");
+            text = text.replace(regex, function (match){
+                let newWord = wordsArray[original]
+                
+                if (match[0]===match[0].toUpperCase()){
+                    newWord = newWord[0].toUpperCase() + newWord.slice(1)
+                }
+                return '<span class="highlight">' + newWord + '</span>';
+            })
+          }
+        return text;
+    }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,34 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      let {text, locale} = req.body;
+      let translated
+      if (text==''){
+        res.json({error: 'No text to translate'})
+      }
+      else if (!locale || !text){
+        res.json({ error: 'Required field(s) missing' })
+      }
+      if (locale==="british-to-american"){
+        //GB to US
+        translated = translator.GBToUS(text)
+        //res.json({text: text, translation: locale})
+      }
+      else if (locale==="american-to-british"){
+        //US to GB
+        translated = translator.USToGB(text)
+        //res.json({text: text, translation: locale})
+      }
+      else {
+        res.json({ error: 'Invalid value for locale field' })
+      }
+      if (translated===text){
+        //if no changes...
+        res.json({text: text, translation: "Everything looks good to me!"})
+      }
+      else{
+        //return translated text
+        res.json({text: text, translation: translated})
+      }
     });
 };
